f008b14c3d8f61d986dd454c6c4381bce5143cfa
Your Name <you@example.com>
2021-04-03 18:20:34 -0300

    jet

--- JetConnection/JetConnection.cs
+++ JetConnection/JetConnection.cs
@@ -3,6 +3,7 @@ using JetConnection.Extensions;
 using JetConnection.Factories;
 using JetConnection.Schemas;
 using JetConnection.Store;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Data;
